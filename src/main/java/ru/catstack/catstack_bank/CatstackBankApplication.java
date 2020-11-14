bd1f51a56e77232ee0b54c333143c9b5ae688c34
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-11-14 20:27:42 +0500

    Initial commit

---
+++ src/main/java/ru/catstack/catstack_bank/CatstackBankApplication.java
@@ -0,0 +1,13 @@
+package ru.catstack.catstack_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatstackBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatstackBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/catstack/catstack_bank/CatstackBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.catstack.catstack_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatstackBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
